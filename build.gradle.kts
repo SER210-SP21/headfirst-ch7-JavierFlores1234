934f5c252b9e19bfe6ccc3cad2fb8b3cd8d08325
JavierFlores1234 <144390422+JavierFlores1234@users.noreply.github.com>
2024-02-16 18:34:44 -0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.library") version "8.2.2" apply false
+
+}
